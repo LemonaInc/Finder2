134970cda12d9f1b361528fac4444e369b1e5e3f
LemonaInc <jaxonstevens@lemonainc.com>
2015-09-19 15:50:58 -0400

    Initial Commit

---
+++ Finder2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Finder2
+//
+//  Created by Jaxon Stevens on 9/19/15.
+//  Copyright © 2015 Jaxon Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Finder2Tests/Finder2Tests.m
@@ -0,0 +1,39 @@
+//
+//  Finder2Tests.m
+//  Finder2Tests
+//
+//  Created by Jaxon Stevens on 9/19/15.
+//  Copyright © 2015 Jaxon Stevens. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Finder2Tests : XCTestCase
+
+@end
+
+@implementation Finder2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
